5350128982105a17748da99997ab2895e2da2c77
Ujjaval Saini <sainiujvl@gmail.com>
2026-02-12 21:34:42 +0530

    Add a new DemoScreen composable to display "Hello World!" text.

---
+++ app/src/main/java/com/uv/quotecomposeapp/ui/screens/DemoScreen.kt
@@ -0,0 +1,14 @@
+package com.uv.quotecomposeapp.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Demo(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello World!",
+        modifier = modifier
+    )
+
+}
